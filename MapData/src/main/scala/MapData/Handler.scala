3ce7aa22f40e11bb62b3a414fa6bee00ab4fee7a
Will Farrell <farrell.will.g@gmail.com>
2019-04-21 12:53:07 -0500

    Read messages from json. Return exception if bucket not found.

--- MapData/src/main/scala/MapData/Handler.scala
+++ MapData/src/main/scala/MapData/Handler.scala
@@ -1,14 +1,48 @@
 package MapData
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-
+import com.amazonaws.services.s3.event.S3EventNotification
 import awscala._
-import com.amazonaws.services.lambda.runtime.events.{S3Event}
+import s3.{Bucket, S3, S3Object}
+import play.api.libs.json._
 
-class Handler extends RequestHandler[S3Event, (String, Error)] {
+class Handler extends RequestHandler[S3EventNotification, (String, Exception)] {
 
-  def handleRequest(input: S3Event, context: Context): (String, Error) = {
-    println("Triggered S3 handle request")
+  def handleRequest(input: S3EventNotification, context: Context): (String, Exception) = {
+    implicit val s3 = S3.at(Region.US_EAST_1)
+
+    val bucketName: String = "farrell-data-engineeri-target"
+
+    val maybeBucket: Option[Bucket] = s3.bucket(bucketName)
+
+    maybeBucket match {
+      case Some(bucket) => {
+        val records = input.getRecords
+
+        // TODO how to use map instead of forEach on something that isn't an array
+        records.forEach((x: S3EventNotification.S3EventNotificationRecord)  => {
+          val key = x.getS3.getObject.getKey
+          val obj: Option[S3Object] = s3.get(bucket, key)
+
+          obj.foreach(o => {
+            val stream = o.content
+            val jsonInput: JsValue = try {  Json.parse(stream) } finally { stream.close() }
+
+            val slackMessages: JsResult[SlackMessages] = jsonInput.validate[SlackMessages]
+
+            slackMessages match {
+              case JsSuccess(slackMessages: SlackMessages, jsPath) => {
+                slackMessages.messages.foreach(println)
+              }
+              case JsError(e) => println(e)
+            }
+          })
+        })
+      }
+      case None => return ("Your function finished executing", new Exception("No bucket found with key " + bucketName))
+    }
+
+
     ("Go Serverless v1.0! Your function executed successfully!", null)
   }
 }
--- MapData/src/main/scala/MapData/SlackMessage.scala
+++ MapData/src/main/scala/MapData/SlackMessage.scala
@@ -1,5 +1,24 @@
 package MapData
 
-class SlackMessage {
+import play.api.libs.json.{JsPath, Reads}
+import play.api.libs.functional.syntax._
 
+
+case class SlackMessage(text: String, user: String)
+
+object SlackMessage {
+  implicit val slackMessageReads: Reads[SlackMessage] = (
+    (JsPath \ "text" ).read[String] and
+      (JsPath \ "user").read[String]
+    )(SlackMessage.apply _)
+}
+
+
+case class SlackMessages(messages: List[SlackMessage], ok: Boolean)
+
+object SlackMessages {
+  implicit val slackMessagesReads: Reads[SlackMessages] = (
+    (JsPath \ "messages" ).read[List[SlackMessage]] and
+      (JsPath \ "ok").read[Boolean]
+    )(SlackMessages.apply _)
 }

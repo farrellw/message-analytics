b9456255b7cf6a37d9925ef74aea77162b26cd02
Will Farrell <farrell.will.g@gmail.com>
2019-04-21 18:06:01 -0500

    Remove unused responses.

--- MapData/src/main/scala/MapData/Handler.scala
+++ MapData/src/main/scala/MapData/Handler.scala
@@ -1,6 +1,6 @@
 package MapData
 
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler, RequestStreamHandler}
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.s3.event.S3EventNotification
 import awscala._, dynamodbv2._
 import s3.{Bucket, S3, S3Object}
@@ -20,16 +20,16 @@ class Handler extends RequestHandler[S3EventNotification, (String, Exception)] {
     val executionFinished: String = "Your function finished executing successfully"
 
     // TODO only access bucket if input.getRecords is not empty
-    val maybeBucket: Option[Bucket] = s3.bucket(bucketName)
+    val bucket: Option[Bucket] = s3.bucket(bucketName)
 
-    maybeBucket match {
-      case Some(bucket) => {
+    bucket match {
+      case Some(b) => {
         val records = input.getRecords
 
 
         records.forEach((x: S3EventNotification.S3EventNotificationRecord) => {
           val key: String = x.getS3.getObject.getKey
-          val obj: Option[S3Object] = s3.get(bucket, key)
+          val obj: Option[S3Object] = s3.get(b, key)
 
           obj match {
             case Some(o) => {
@@ -55,17 +55,17 @@ class Handler extends RequestHandler[S3EventNotification, (String, Exception)] {
                           val messages = slackMessages.messages
                           messages.foreach(m => t.put(m.user, m.ts, "Text" -> m.text))
                         }
-                        case None => (executionFinishedError, new Exception("Cannot find table name " + tableName))
+                        case None => return (executionFinishedError, new Exception("Cannot find table name " + tableName))
                       }
 
                     }
-                    case Failure(e) => (executionFinishedError, new Exception("Failed connecting to dynamoDB"))
+                    case Failure(e) => return (executionFinishedError, new Exception("Failed connecting to dynamoDB"))
                   }
                 }
-                case JsError(e) => (executionFinishedError, new Exception("An error occurred validating JSON"))
+                case JsError(e) => return (executionFinishedError, new Exception("An error occurred validating JSON"))
               }
             }
-            case None => (executionFinishedError, new Exception("Error Accessing key " + key))
+            case None => return (executionFinishedError, new Exception("Error Accessing key " + key))
           }
         })
         (executionFinished, null)
